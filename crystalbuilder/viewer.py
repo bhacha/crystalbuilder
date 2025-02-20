12237786d0362bf74595e78ef8127cf08efc4837
bhacha <29590546+bhacha@users.noreply.github.com>
2025-02-19 18:29:17 -0600

    updated for core-shell

--- crystalbuilder/viewer.py
+++ crystalbuilder/viewer.py
@@ -5,7 +5,7 @@ import crystalbuilder.geometry as geo
 vedo.settings.default_backend='vtk'
 
 
-def visualize(structures, plotter_style=9):
+def visualize(structures, plotter_style=9, **kwargs):
     """
     
     Parameters
@@ -20,35 +20,33 @@ def visualize(structures, plotter_style=9):
     for object in structures:
         
         if isinstance(object, geo.Cylinder):
-            plot += visualize_cylinder(object)
+            plot += visualize_cylinder(object, **kwargs)
         
-        elif isinstance(object, geo.Sphere):
-            plot += visualize_sphere(object)
-
         elif isinstance(object, geo.SuperCell):
-            plot += visualize_supercell(object)
+            plot += visualize_supercell(object, **kwargs)
 
-
-    plot.show().close()
     return plot
 
-def visualize_cylinder(cylinder):
+def add_to_visualizer(structures, plot, **kwargs):
+    for object in structures:       
+        if isinstance(object, geo.Cylinder):
+            plot += visualize_cylinder(object, **kwargs)
+        elif isinstance(object, geo.SuperCell):
+            plot += visualize_supercell(object, **kwargs)
+
+
+def visualize_cylinder(cylinder, **kwargs):
     center = cylinder.center
     radius = cylinder.radius
     height = cylinder.height
     axis = cylinder.axis
-    return vedo.Cylinder(pos=center, r=radius, height=height, axis=axis)
+    return vedo.Cylinder(pos=center, r=radius, height=height, axis=axis, **kwargs)
 
-def visualize_sphere(sphere):
-    center = sphere.center
-    radius = sphere.radius
-    return vedo.Sphere(pos=center, r=radius)
-
-def visualize_supercell(SuperCell):
+def visualize_supercell(SuperCell, **kwargs):
     objects = []
     for structure in SuperCell:
         if isinstance(structure, geo.Cylinder):
-            objects.append(visualize_cylinder(structure))
+            objects.append(visualize_cylinder(structure, **kwargs))
     return objects
 
 
