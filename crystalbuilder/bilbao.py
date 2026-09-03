fixed a_mag scaling issues with bilbao structures

crystalbuilder/bilbao.py
@@ -330,7 +330,9 @@ class SpaceGroup():
                     generated_points = np.unique(generated_points, axis=0)
                 
             else:
-                generated_points:array = get_coordinates(self.group_num, origin=point_list)
+                scaled_point = point_list * np.asarray(a_mag)
+                print(scaled_point)
+                generated_points:array = get_coordinates(self.group_num, origin=scaled_point, a_mag=a_mag)
                 generated_points.reshape(-1,3)
                 listlen=len(generated_points)
                 if ignore_repeats == True:

crystalbuilder/conversions/t3d.py
@@ -51,7 +51,7 @@ def rotate_to(orientation, v1 = [0,0,1]):
     
     rot_axis_unnorm = np.cross(v1, orientation)
     s_angle = np.linalg.norm(rot_axis_unnorm)
-    rot_axis = rot_axis_unnorm/s_angle
+    rot_axis = rot_axis_unnorm/s_angle 
     c_angle = np.dot(v1, orientation)
     rotmat2 = Transformed.rotation(s_angle, (rot_axis[0], rot_axis[1], rot_axis[2]))
 

crystalbuilder/geometry.py
@@ -990,11 +990,11 @@ def NearestNeighbors(points, radius, neighborhood_range, a_mag=1.0):
     """
     pointarr = np.asarray(points)
     kdtree = scs.KDTree(pointarr, leafsize=15, compact_nodes=True)
-    neighbors = kdtree.query_pairs(r=neighborhood_range, p=2)
+    neighbors = kdtree.query_pairs(r=neighborhood_range*a_mag, p=2)
     structure_list = []
     for pair in neighbors:
-        point = pointarr[pair[0]] * a_mag
-        neighbor = pointarr[pair[1]] *a_mag
+        point = pointarr[pair[0]]
+        neighbor = pointarr[pair[1]]
         structure_list.append(Cylinder.from_vertices([point, neighbor], radius=radius))
     return structure_list
 