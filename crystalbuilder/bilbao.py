actually fixed bilbao scaling issues, fixed inexplicable indexing error when defining cylinder axis

crystalbuilder/bilbao.py
@@ -244,7 +244,7 @@ def get_genmat(groupnum:int, output:cbt.Literal['array', 'list', None]=None, **k
         return
     
 
-def get_coordinates(groupnum:int, origin:cbt.VectorType, output:cbt.Literal['array', 'list']='array', a_mag:cbt.VectorType = np.array([1,1,1]), include_out_of_bounds=False, **kwargs) -> list|cbt.Array|None:
+def get_coordinates(groupnum:int, origin:cbt.VectorType, output:cbt.Literal['array', 'list']='array', a_mag:cbt.VectorType = np.array([1,1,1]), include_out_of_bounds=True, **kwargs) -> list|cbt.Array|None:
     """Generates positions from specified origin and generator matrices
 
     Parameters

crystalbuilder/geometry.py
@@ -456,7 +456,7 @@ class Cylinder(Structure):
                 self.axis=np.array([1, 0, 0])
         except ValueError:
             try:
-                self.axis = self.inaxis[:2] # take the first three values as a three vector
+                self.axis = self.inaxis[:3] # take the first three values as a three vector. I have no idea why the indexing needs to be like this. 
             except ValueError:
                 raise Exception("Error: Axis not specified correctly.")
         